229494af4fbdcfddb6a43dd16d9434c567564075
tapu-gugu <tapu-gugu@users.noreply.github.com>
2026-03-24 14:57:55 +0000

    deploy: 0e3b7036cd10cc0aa93a522245fa026ca4af1970

--- assets/py/节奏-C2NzxGpI.py
+++ assets/py/节奏-C2NzxGpI.py
@@ -76,17 +76,24 @@ def calculate_rhythm_difficulty(arr, T):
                 smaller = min(ai, ai_prev)
                 ratio = larger / smaller
                 N = ratio - int(ratio)  # 取小数部分
-
-            # a_coeff = 2 * math.sqrt(0.25 - (0.5 - N) ** 2)
         
             # 修改1: 将N赋值为N和1-N中的较小值
             N = min(N, 1 - N)
         
             # 修改2: 根据N值使用不同的计算公式
             if N < 1/3:
-                a_coeff = 2 * math.sqrt(0.25 - (0.5 * (1 - 3 * N)) ** 2)
+                a_coeff1 = 2 * math.sqrt(0.25 - (0.5 * (1 - 3 * N)) ** 2)
             else:
-                a_coeff = 2 * math.sqrt(0.25 - (0.9 * (1 - 3 * N)) ** 2)
+                a_coeff1 = 2 * math.sqrt(0.25 - (0.9 * (1 - 3 * N)) ** 2)
+
+            delta = larger - smaller
+            a_coeff2 = 0
+
+            if delta < 50:
+                delta = min(delta, 50 - delta)
+                a_coeff2 = delta / 25
+
+            a_coeff = 1 - (1 - a_coeff1) * (1 - a_coeff2)
             
             # 计算b系数（大间隔修正系数乘积）
             def get_interval_coeff(value):
